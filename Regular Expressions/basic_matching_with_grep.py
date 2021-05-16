6b3bcb541c94f466f22f79e1a4fbb83fd4b4e997
Gillian Weisgram <trekker35@gmail.com>
2021-05-16 13:22:11 -0700

    finished regular expressions section

--- Regular Expressions/basic_matching_with_grep.py
+++ Regular Expressions/basic_matching_with_grep.py
@@ -1,2 +1,15 @@
 import re
 
+#grep - easily try out expressions and gain familiarity
+#search to see if a string is present
+#find text in /usr/share/dict/words, for example:
+#grep thon
+#case sensitive
+#grep -i matches in a case-insentitive way
+#reserved characters allow us to do more advanced matching:
+
+#   . matches any character
+#   ^ or $ match to the beginning or end of word:
+#grep ^fruit would retrun all words beginning with fruit
+#grep cat$ would retun words ending with cat
+
