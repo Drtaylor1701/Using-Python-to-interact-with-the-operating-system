c03931eb6b3055fc48fa5225ea8b044377ef2796
drtaylor1701 <trekker35@gmail.com>
2021-12-25 19:45:01 -0800

    finished a pom of this

--- Final Project/log_reader.py
+++ Final Project/log_reader.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+'''opens a log file and reads each line, collecting information about each log.'''
+import re
+
+error_dict = dict()
+
 with open('Final Project/syslog.log', 'r') as logfile:
     for line in logfile:
-        print(line, end='')
+        #print(line, end='')
+        LOGTYPE = r'(INFO|ERROR)'
+        log_type = re.search(LOGTYPE, line)
+        log_type = log_type[1]
+        if log_type == 'ERROR':
+            #print(line)
+            error_text_finder = r'(ERROR )([\D|\s]*)\('
+            error_text = re.search(error_text_finder, line)
+            print(error_text[2])
+        elif log_type == 'INFO':
+            info_text_finder = r'(INFO )([\D|\s]*)\('
+            info_text = re.search(info_text_finder, line)
+            print(info_text)
