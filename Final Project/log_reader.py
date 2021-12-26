8112152b7e8070ad1838792fc933dae800a9bcda
drtaylor1701 <trekker35@gmail.com>
2021-12-25 19:24:58 -0800

    started over on log reader; got real log file

--- Final Project/log_reader.py
+++ Final Project/log_reader.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 
-import re
-
-with open("/Users/gillian/Dropbox/My Mac (gillian-FVFF9334Q6LT.local)/Documents/Using-Python-to-interact-with-the-operating-system/Final Project/syslog_sample.txt", "r") as log_file
-
-
-def split_up_message(log_file):
-    for row in log_file:
-        ticketType = re.search(r"(INFO|ERROR)", row)
-        print(ticketType[0])
-
-
-def main():
-    split_up_message()
-
-
-if __name__ == "__main__":
-    main()
+with open('Final Project/syslog.log', 'r') as logfile:
+    for line in logfile:
+        print(line, end='')
--- Final Project/log_reader_test.py
+++ Final Project/log_reader_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from unittest.case import TestCase
-from log_reader import split_up_message
+from log_reader import findTicketType
+from log_reader import captureErrorMessage
 import unittest
 
 
@@ -9,12 +10,18 @@ class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "May 27 11:45:40 ubuntu.local.ticky: INFO: Created ticket [#1234] (username)"
         expected = "INFO"
-        self.assertEqual(split_up_message(testcase), expected)
+        self.assertEqual(findTicketType(testcase), expected)
 
-    def test_empty(self):
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
         testcase = "Jun 1 11:46:48 ubuntu.local.ticky: ERROR: Connection to DB failed (username)"
         expected = "ERROR"
-        self.assertEqual(split_up_message(testcase), expected)
+        self.assertEqual(findTicketType(testcase), expected)
 
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Jun 1 11:46:48 ubuntu.local.ticky: ERROR: Connection to DB failed (username)"
+        expected = "Connection to DB failed"
+        self.assertEqual(captureErrorMessage(testcase), expected)
 
 unittest.main()
