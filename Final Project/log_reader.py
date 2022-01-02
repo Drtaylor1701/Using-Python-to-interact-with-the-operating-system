c3594483c6b1de73c61d29bd9fa69836cf7a20bf
drtaylor1701 <trekker35@gmail.com>
2022-01-02 09:04:46 -0800

    made progress on log reader script

--- Final Project/log_reader.py
+++ Final Project/log_reader.py
@@ -1,22 +1,77 @@
 #!/usr/bin/env python3
-
-'''opens a log file and reads each line, collecting information about each log.'''
 import re
+import csv
 
-error_dict = dict()
+LOGTYPE = r'(ERROR|INFO)'
+USERNAME = r'\(([\w.]*)\)$'
+ERRORTEXT = r'ERROR ([\D]*) \('
+error_dict = {}
+user_dict = {}
 
-with open('Final Project/syslog.log', 'r') as logfile:
-    for line in logfile:
-        #print(line, end='')
-        LOGTYPE = r'(INFO|ERROR)'
-        log_type = re.search(LOGTYPE, line)
-        log_type = log_type[1]
-        if log_type == 'ERROR':
-            #print(line)
-            error_text_finder = r'(ERROR )([\D|\s]*)\('
-            error_text = re.search(error_text_finder, line)
-            print(error_text[2])
-        elif log_type == 'INFO':
-            info_text_finder = r'(INFO )([\D|\s]*)\('
-            info_text = re.search(info_text_finder, line)
-            print(info_text)
+def read_ticket_type(line):
+    '''reads log file and detects ticket type'''
+    ticket_type = re.search(LOGTYPE, line)
+    ticket_type = ticket_type[1]
+    return ticket_type
+
+def read_user_name(line):
+    '''reads log file and detects user name'''
+    user_name = re.search(USERNAME, line)
+    user_name = user_name[1]
+    return user_name
+
+def read_error_text(line):
+    '''reads error text from error line'''
+    error_text = re.search(ERRORTEXT, line)
+    error_text = error_text[1]
+    #print(error_text)
+    return error_text
+
+def add_dict_entry(key_entry, dict_name):
+    if key_entry in dict_name:
+        dict_name[key_entry] += 1
+    else:
+        dict_name[key_entry] = 1
+    print(dict_name)
+    return dict_name
+
+def sort_dict_by_value(dict_name):
+    sorted_dict = {}
+    for entry in sorted(dict_name, key=dict_name.get, reverse=True):
+        sorted_dict[entry] = dict_name[entry]
+    #print(sorted_dict)
+    return sorted_dict
+
+def sorted_dict_by_key(dict_name):
+    dict_to_sort = dict_name.items()
+    sorted_dict = sorted(dict_to_sort)
+    #print(sorted_dict)
+    return sorted_dict
+
+def convert_dict_to_csv(input_dict, csvname, col_list):
+    try:
+        with open(csvname, 'x', encoding="utf-8") as f:
+            fieldnames = col_list
+            dictwriter = csv.DictWriter(f, fieldnames=col_list)
+            writer = csv.DictWriter(f, fieldnames)
+            dictwriter.writerow(fieldnames)
+            for entry in input_dict:
+                writer.writerow(entry)
+    except IOError:
+        return "Write error"
+
+with open('Final Project/syslog.log', 'r', encoding="utf-8") as logfile:
+
+    for log in logfile:
+        #print(read_ticket_type(log))
+        #print(read_user_name(log))
+        if read_ticket_type(log) == 'ERROR':
+            key_entry = read_error_text(log)
+            #print(key_entry)
+            add_dict_entry(key_entry, error_dict)
+            #print(error_dict)
+            sorted_error_dict = sort_dict_by_value(error_dict)
+            convert_dict_to_csv(sorted_error_dict, "errors.csv", ['Error Type', 'Occurrences'])
+        user_name = read_user_name(log)
+        add_dict_entry(user_name, user_dict)
+        sorted_dict_by_key(user_dict)
--- Final Project/log_reader_test.py
+++ Final Project/log_reader_test.py
@@ -1,27 +1,76 @@
 #!/usr/bin/env python3
-
-from unittest.case import TestCase
-from log_reader import findTicketType
-from log_reader import captureErrorMessage
+'''unit tests for log reader script'''
 import unittest
+from log_reader import read_ticket_type
+from log_reader import read_user_name
+from log_reader import read_error_text
+from log_reader import add_dict_entry
+from log_reader import sort_dict_by_value
+from log_reader import sorted_dict_by_key
+from log_reader import convert_dict_to_csv
 
-
-class TestRearrange(unittest.TestCase):
-    def test_basic(self):
-        testcase = "May 27 11:45:40 ubuntu.local.ticky: INFO: Created ticket [#1234] (username)"
+class TestTicketTyping(unittest.TestCase):
+    '''unit tests for reading logs'''
+    def test_basic_info(self):
+        '''tests a basic info log'''
+        testcase = "Jan 31 00:09:39 ubuntu.local ticky: INFO Created ticket [#4217] (mdouglas)"
         expected = "INFO"
-        self.assertEqual(findTicketType(testcase), expected)
+        self.assertEqual(read_ticket_type(testcase), expected)
 
-class TestRearrange(unittest.TestCase):
-    def test_basic(self):
-        testcase = "Jun 1 11:46:48 ubuntu.local.ticky: ERROR: Connection to DB failed (username)"
+    def test_basic_error(self):
+        '''tests a basic error log'''
+        testcase = "Jan 31 00:21:30 ubuntu.local ticky: ERROR The ticket was modified while updating (breee)"
         expected = "ERROR"
-        self.assertEqual(findTicketType(testcase), expected)
+        self.assertEqual(read_ticket_type(testcase), expected)
 
-class TestRearrange(unittest.TestCase):
-    def test_basic(self):
-        testcase = "Jun 1 11:46:48 ubuntu.local.ticky: ERROR: Connection to DB failed (username)"
-        expected = "Connection to DB failed"
-        self.assertEqual(captureErrorMessage(testcase), expected)
+class UserName(unittest.TestCase):
+    '''unit tests for reading logs'''
+    def test_basic_uname(self):
+        '''tests a basic info username'''
+        testcase = "Jan 31 00:09:39 ubuntu.local ticky: INFO Created ticket [#4217] (mdouglas)"
+        expected = "mdouglas"
+        self.assertEqual(read_user_name(testcase), expected)
 
-unittest.main()
+    def test_user_name_with_dot(self):
+        '''tests a username with a dot in it'''
+        testcase = "Jan 31 04:30:04 ubuntu.local ticky: ERROR Permission denied while closing ticket (rr.robinson)"
+        expected = "rr.robinson"
+        self.assertEqual(read_user_name(testcase), expected)
+
+class ErrorText(unittest.TestCase):
+    def test_error_message(self):
+        '''test recording error text'''
+        testcase = "Jan 31 00:21:30 ubuntu.local ticky: ERROR The ticket was modified while updating (breee)"
+        expected = "The ticket was modified while updating"
+        self.assertEqual(read_error_text(testcase), expected)
+
+class UpdateDict(unittest.TestCase):
+    def test_dictionary_update(self):
+        '''test recording error text'''
+        testcase1 = "The ticket was modified while updating"
+        testcase2 = {}
+        expected = {'The ticket was modified while updating': 1}
+        self.assertEqual(add_dict_entry(testcase1, testcase2), expected)
+
+class SortDictByValue(unittest.TestCase):
+    def test_dictionary_update(self):
+        '''test recording error text'''
+        testcase1 = {'The ticket was modified while updating': 9, 'Permission denied while closing ticket': 10, 'Tried to add information to closed ticket': 12, 'Timeout while retrieving information': 15, "Ticket doesn't exist": 7, 'Connection to DB failed': 13}
+        expected = {'Timeout while retrieving information': 15, 'Connection to DB failed': 13, 'Tried to add information to closed ticket': 12, 'Permission denied while closing ticket': 10, 'The ticket was modified while updating': 9, "Ticket doesn't exist": 7}
+        self.assertEqual(sort_dict_by_value(testcase1), expected)
+
+class SortDictByKey(unittest.TestCase):
+    def test_dictionary_update(self):
+        '''test sorting a dictionary by key'''
+        testcase1 = {'mdouglas': 5, 'noel': 9, 'breee': 6, 'ac': 4, 'blossom': 8, 'rr.robinson': 3, 'mcintosh': 7, 'jackowens': 6, 'oren': 9, 'xlg': 4, 'ahmed.miller': 6, 'bpacheco': 2, 'enim.non': 5, 'flavia': 5, 'sri': 4, 'nonummy': 5, 'britanni': 2, 'montanap': 4, 'mai.hendrix': 3, 'kirknixon': 3}
+        expected = [('ac', 4), ('ahmed.miller', 6), ('blossom', 8), ('bpacheco', 2), ('breee', 6), ('britanni', 2), ('enim.non', 5), ('flavia', 5), ('jackowens', 6), ('kirknixon', 3), ('mai.hendrix', 3), ('mcintosh', 7), ('mdouglas', 5), ('montanap', 4), ('noel', 9), ('nonummy', 5), ('oren', 9), ('rr.robinson', 3), ('sri', 4), ('xlg', 4)]
+        self.assertEqual(sorted_dict_by_key(testcase1), expected)
+
+class Create_Csv(unittest.TestCase):
+    def test_dict_to_csv_conversion(self):
+        '''test sorting a dictionary by key'''
+        testcase1 = {'mdouglas': 5, 'noel': 9, 'breee': 6, 'ac': 4, 'blossom': 8, 'rr.robinson': 3, 'mcintosh': 7, 'jackowens': 6, 'oren': 9, 'xlg': 4, 'ahmed.miller': 6, 'bpacheco': 2, 'enim.non': 5, 'flavia': 5, 'sri': 4, 'nonummy': 5, 'britanni': 2, 'montanap': 4, 'mai.hendrix': 3, 'kirknixon': 3}
+        expected = 'file written'
+        self.assertEqual(convert_dict_to_csv(testcase1), expected)
+
+#unittest.main()
